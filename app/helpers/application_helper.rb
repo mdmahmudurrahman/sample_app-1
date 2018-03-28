63e71fe1b86ec61f6871319886d530bebcaf0654
Hoang Hai <hoanghai18497@gmail.com>
2018-03-28 13:32:44 +0700

    Add a Static Pages controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title page_title = ""
+    page_title.empty? ? I18n.t("rortas") : page_title + " | " + I18n.t("rortas")
+  end
 end
